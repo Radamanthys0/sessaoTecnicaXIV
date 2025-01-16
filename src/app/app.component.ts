d89c7f98affaa387361f20033939a2974382ff9c
Radamanthys0 <ludwigadamo@gmail.com>
2025-01-15 23:26:47 -0300

    feat: add Update on signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,18 @@ import { Component, computed, effect, signal } from '@angular/core';
 export class AppComponent {
 
   counter = signal(0)
+
   setValue() {
     this.counter.set(10)
+  }
+
+  decrease() {
+    // this.counter.update(c => --c)
+    this.counter.update(c => c > 0 ? --c : 0)
+  }
+
+  increase() {
+    this.counter.update(c => ++c)
   }
 
 }
