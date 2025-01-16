0c2f1049e5a3e04580d841ec7f706e3ddfbe0eb4
Radamanthys0 <ludwigadamo@gmail.com>
2025-01-16 00:13:43 -0300

    feat: creating components states

--- src/app/coin/coin.component.ts
+++ src/app/coin/coin.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-coin',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './coin.component.html',
   styleUrl: './coin.component.scss'
 })
 export class CoinComponent {
-
+  label = signal('label')
+  value = signal(10)
 }
