b80a8959c3e42e6c2644e56e9e5cb751f5921cd7
Radamanthys0 <ludwigadamo@gmail.com>
2025-01-15 23:08:43 -0300

    feat: create counter signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  counter = signal(0)
 
 }
